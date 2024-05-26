8bf5be2546a2f12f81a9097011e8d945688de7e8
Sojin Antony <Sojin.Antony@ibm.com>
2024-05-26 18:21:21 +0530

    PWA app for showing my git issues and details

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Octokit }  from "octokit"
+
 import './App.css';
+import config from './config';
 
 function App() {
+  const [issues, setIssues] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+    // Octokit.js
+    // https://github.com/octokit/core.js#readme
+      const  octokit = new Octokit({
+        // auth: config.token
+      })
+
+      const values = await octokit.request('GET /repos/sojinantony01/react-cron-generator/issues', {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        },
+        filter: "all"
+      })
+      setIssues(values.data)
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {issues.map(d => 
+          <div class="card">
+          <div class="container">
+            {/* <h4><b>{d.repository.name}</b></h4> */}
+            <div><a href={d.url} >{d.title}</a> </div>
+            <div><p>{d.assignee}</p> <p>{d.status}</p></div> 
+            <div><a href={d.comments_url}>Comments</a></div>
+          </div>
+        </div>
+        )}
+        
     </div>
   );
 }
